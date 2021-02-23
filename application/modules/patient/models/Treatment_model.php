fda268579a286ed324d53ac0c3db7395fbbc9263
Shivaraj B <shivaraj2badiger@gmail.com>
2021-02-23 12:32:46 +0530

    ipd fixes
    Signed-off-by: Shivaraj B <shivaraj2badiger@gmail.com>

--- application/modules/patient/models/Treatment_model.php
+++ application/modules/patient/models/Treatment_model.php
@@ -193,7 +193,7 @@ class treatment_model extends CI_Model {
     public function admit_patient($inpatientdata) {
         if (!empty($inpatientdata)) {
             $query = "INSERT INTO inpatientdetails (OpdNo, deptOpdNo, FName, Age, Gender, department, BedNo, diagnosis, DoAdmission, Doctor,treatId) 
-                SELECT T.OpdNo,T.deptOpdNo,CONCAT(FirstName,' ',MidName,' ',LastName),Age,Gender,T.department,'" . $inpatientdata['BedNo'] . "',T.diagnosis,'" . $inpatientdata['DoAdmission'] . "',T.attndedby,'" . $inpatientdata['treatId'] . "' 
+                SELECT T.OpdNo,T.deptOpdNo,CONCAT(FirstName,' ',LastName) as name,Age,Gender,T.department,'" . $inpatientdata['BedNo'] . "',T.diagnosis,'" . $inpatientdata['DoAdmission'] . "',T.attndedby,'" . $inpatientdata['treatId'] . "' 
                     FROM patientdata P JOIN treatmentdata T ON P.OpdNo=T.OpdNo WHERE P.OpdNo='" . $inpatientdata['OpdNo'] . "' and T.ID='" . $inpatientdata['treatId'] . "'";
             $this->db->query($query);
             $insert_id = $this->db->insert_id();
