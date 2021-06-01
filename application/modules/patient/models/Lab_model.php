7029d7861a65e95ce4b8edb66156fb5d29cc25a4
Shivaraj B <shivaraj2badiger@gmail.com>
2021-06-01 21:32:11 +0530

    search index
    Signed-off-by: Shivaraj B <shivaraj2badiger@gmail.com>

--- application/modules/patient/models/Lab_model.php
+++ application/modules/patient/models/Lab_model.php
@@ -32,8 +32,7 @@ class Lab_model extends CI_Model {
         $search_data = $this->input->post('search');
         $search_value = trim($search_data['value']);
         $search = (isset($search_data) && $search_value != '') ? 'AND (l.OpdNo like "%' . $search_value . '%" 
-            OR (REPLACE(ucfirst(t.department),"_"," ")) like "%' . $search_value . '%" 
-                OR t.diagnosis like "%' . $search_value . '%" OR FirstName like "%' . $search_value . '%") ' : '';
+            OR (REPLACE(ucfirst(t.department),"_"," ")) like "%' . $search_value . '%" ) ' : '';
         $where_cond = " WHERE ((tested_date is NULL) OR ( testrange is NULL AND testvalue IS NULL) OR (testDate='$cur_date')) $search"; //xrayDate is NULL AND filmSize is NULL
         if (!$export_flag) {
             $start = (isset($conditions['start'])) ? $conditions['start'] : 0;
