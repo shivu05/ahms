4576802fb099ee3ea1452664f812a042d242954a
Shivaraj B <shivaraj2badiger@gmail.com>
2021-09-05 22:30:31 +0530

    added security policy
    Signed-off-by: Shivaraj B <shivaraj2badiger@gmail.com>

--- application/core/SHV_Controller.php
+++ application/core/SHV_Controller.php
@@ -11,15 +11,17 @@
  *
  * @author Shivaraj
  */
-class SHV_Controller extends MX_Controller {
+class SHV_Controller extends MX_Controller
+{
 
-    protected $data = Array(); //protected variables goes here its declaration
+    protected $data = array(); //protected variables goes here its declaration
     private $Username;
     private $Password;
     private $Config;
     private $Ipaddress;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
         $this->load->model('home/settings_model');
         $this->load->library('REST_Client');
@@ -32,7 +34,8 @@ class SHV_Controller extends MX_Controller {
      * @desc: To initialize rest client for both dvs and ems
      * @author: Shivaraj B
      */
-    public function initialize_rest_client() {
+    public function initialize_rest_client()
+    {
 
         $this->Ipaddress = 'localhost';
         $this->Config = array(
@@ -46,12 +49,14 @@ class SHV_Controller extends MX_Controller {
         $this->rest_client->initialize($this->Config);
     }
 
-    function application_settings($columns = NULL) {
+    function application_settings($columns = NULL)
+    {
         $data['app_settings'] = $this->settings_model->get_configuration_settings($columns);
         return $data['app_settings'];
     }
 
-    function get_department_list($response_type = 'json') {
+    function get_department_list($response_type = 'json')
+    {
         $this->load->model('master/department_model');
         $departments = $this->department_model->get_departments();
         if ($response_type == 'json') {
@@ -62,7 +67,8 @@ class SHV_Controller extends MX_Controller {
         }
     }
 
-    function fetch_panchakarma_procedures() {
+    function fetch_panchakarma_procedures()
+    {
         $this->load->model('master/panchakarma_model');
         $data = $this->panchakarma_model->get_procedures();
         if ($this->input->is_ajax_request()) {
@@ -72,7 +78,8 @@ class SHV_Controller extends MX_Controller {
         }
     }
 
-    function get_sub_department() {
+    function get_sub_department()
+    {
         $this->load->model('master/department_model');
         $dept_id = $this->input->post('dept_id');
         $return['sub_dept'] = $this->department_model->get_sub_departments($dept_id);
@@ -80,17 +87,20 @@ class SHV_Controller extends MX_Controller {
         echo json_encode($return);
     }
 
-    function get_doctors($dept = null) {
+    function get_doctors($dept = null)
+    {
         $dept_name = (empty($this->input->post('dept_id'))) ? $dept : $this->input->post('dept_id');
         return $this->settings_model->get_doctors_by_dept($dept_name);
     }
 
-    function get_roles() {
+    function get_roles()
+    {
         $this->db->where('role_code !=', 'ADMIN');
         return $this->db->get('role_master')->result_array();
     }
 
-    function get_next_dept_opd($dept) {
+    function get_next_dept_opd($dept)
+    {
         $dept_max_id = $this->db->select_max('deptOpdNo')->where('department', $dept)->get('treatmentdata');
         if (empty($dept_max_id->row()->deptOpdNo)) {
             return 1;
@@ -99,24 +109,40 @@ class SHV_Controller extends MX_Controller {
         }
     }
 
-    function get_packaging_types() {
+    function get_packaging_types()
+    {
         return $this->settings_model->get_packaging_types();
     }
 
-    function get_uuid() {
+    function get_uuid()
+    {
         return $this->uuid->v4();
     }
 
-    function get_medicine_frequency() {
+    function get_medicine_frequency()
+    {
         return $this->settings_model->get_medicine_frequency();
     }
 
-    function get_product_list() {
+    function get_product_list()
+    {
         return $this->settings_model->get_medicine_list();
     }
 
-    function get_configuration_variable($var_name = '') {
+    function get_configuration_variable($var_name = '')
+    {
         return $this->settings_model->get_config_variable_value($var_name);
     }
 
+    function get_first_last_day_of_month($month = "")
+    {
+        if (!empty($month) && $month != '') {
+            $query = "SELECT 
+            DATE_ADD(DATE_ADD(LAST_DAY(STR_TO_DATE('2021-March-01','%Y-%M-%d')),
+                    INTERVAL 1 DAY),
+                INTERVAL - 1 MONTH) AS first_day,LAST_DAY(STR_TO_DATE('2021-March-01','%Y-%M-%d')) last_day";
+            return $result = $this->db->query($query)->row_array();
+        }
+        return false;
+    }
 }
