0ddc2322a66bfb8b169eb231b96498a181648832
Shivaraj B <shivaraj2badiger@gmail.com>
2021-10-25 23:11:30 +0530

    config
    Signed-off-by: Shivaraj B <shivaraj2badiger@gmail.com>

--- application/config/config.php
+++ application/config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
-ini_set('session.cookie_samesite', 'None;Secure');
-ini_set('session.cookie_secure', TRUE);
+//ini_set('session.cookie_samesite', 'None;Secure');
+//ini_set('session.cookie_secure', TRUE);
 /*
   |--------------------------------------------------------------------------
   | Base Site URL
