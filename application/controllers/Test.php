75a50f5c40dced3f32d8b828a619177e2a846a19
Shivaraj Badiger <shivaraj2badiger@gmail.com>
2024-05-11 19:07:45 +0530

    test file updates
    Signed-off-by: Shivaraj Badiger <shivaraj2badiger@gmail.com>

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -143,7 +143,7 @@ class Test extends SHV_Controller {
         if (!empty($db_list)) {
             foreach ($db_list as $row) {
                 echo 'for DB: ' . $row['db_name'] . '--!';
-                $perm_params = array(
+                /*$perm_params = array(
                     'perm_code' => 'BED_MANAGEMENT',
                     'perm_desc' => 'Bed management',
                     'perm_order' => 10,
@@ -164,17 +164,9 @@ class Test extends SHV_Controller {
                     'last_updated_id' => 1,
                     'access_perm' => 2
                 );
-                $role_perm = $this->db->insert('role_perm', $role_params);
-                /*             $alter = $this->db->query("CREATE TABLE `autoclave_register` (
-                  `id` int(11) NOT NULL AUTO_INCREMENT,
-                  `DrumNo` varchar(50) DEFAULT NULL,
-                  `DrumStartTime` datetime DEFAULT NULL,
-                  `DrumEndTime` datetime DEFAULT NULL,
-                  `SupervisorName` varchar(150) DEFAULT NULL,
-                  `Remarks` text DEFAULT NULL,
-                  PRIMARY KEY (`id`)
-                  ) ENGINE=InnoDB AUTO_INCREMENT=1"); */
-                if ($perm_master && $role_perm):
+                $role_perm = $this->db->insert('role_perm', $role_params);*/
+                $alter = $this->db->query("update bed_details set OpdNo=NULL,treatID=NULL,IpNo=NULL where bedstatus='Available'");
+                if ($alter):
                     echo 'Executed on ' . $row['db_name'] . ' at ' . date('dd-mm-YYYY hh:mm:ss') . '</br>';
                 else:
                     echo 'Failed on ' . $row['db_name'] . ' at ' . date('dd-mm-YYYY hh:mm:ss') . '</br>';
